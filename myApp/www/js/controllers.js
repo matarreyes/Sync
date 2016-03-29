026aed9cb53098dc8af9dd659e807559b27f49f5
alejandroMM <alejandromm93@gmail.com>
2016-03-29 20:28:24 +0200

    tabbed tests

---
+++ myApp/www/js/controllers.js
@@ -0,0 +1,67 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+.controller('PlaylistsCtrl', function($scope, $q, $PouchDBListener) {
+    $PouchDBListener.startListening();
+
+    $q.when(
+      localDB.get('plain_text2')
+    ).then(function(doc){
+        $scope.playlists = doc.rows;
+    }).catch(function (err){
+      console.log(err);
+    });
+
+    /**
+     * Puts the new data when changes
+     */
+    $scope.$on('$PouchDBListener:change', function(event, data) {
+      if(data.id == 'plain_text2') {
+        $scope.playlists = data.doc.rows;
+        $scope.$apply();
+      }
+    });
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+});
